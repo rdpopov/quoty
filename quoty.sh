99d4f32b40ef68a2c806a1022c771ac14275cdce
Rosen Popov <rdonkow@gmail.com>
2021-05-16 17:05:44 +0300

    Added some quotes form "Mistborn era 1, book 2 : The we;; of ascension"

--- quoty.sh
+++ quoty.sh
@@ -2,4 +2,4 @@
 quotes_file=~/.config/quoty/quotes
 n_quotes=$(wc -l $quotes_file|cut -d" " -f1)
 rand_quote=$(shuf -i 1-$n_quotes|head -n 1)
-echo "\n\033[3m$(head -n $rand_quote $quotes_file|tail -n 1|fold -s -w 60 )\033[0m\n"
+echo "$(head -n $rand_quote $quotes_file|tail -n 1|fold -s -w 60 )"
