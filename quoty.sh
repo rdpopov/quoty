d04036503b47f271cd615cca3698e51625f701a7
Rosen Popov <rdonkow@gmail.com>
2021-09-23 03:01:26 +0300

    Added a new line after quote is printed

--- quoty.sh
+++ quoty.sh
@@ -3,3 +3,4 @@ quotes_file=~/.config/quoty/quotes
 n_quotes=$(wc -l $quotes_file|cut -d" " -f1)
 rand_quote=$(shuf -i 1-$n_quotes|head -n 1)
 echo "$(head -n $rand_quote $quotes_file|tail -n 1|fold -s -w 60 )"
+echo
