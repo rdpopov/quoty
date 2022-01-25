66b3a01c8d91f4405a9a3e969753bd83789b493f
Rosen Popov <rdonkow@gmail.com>
2022-01-25 02:02:31 +0200

    Added some quotes from the shadows of self

--- quoty.sh
+++ quoty.sh
@@ -2,5 +2,5 @@
 quotes_file=~/.config/quoty/quotes
 n_quotes=$(wc -l $quotes_file|cut -d" " -f1)
 rand_quote=$(shuf -i 1-$n_quotes|head -n 1)
-awk 'NR=='$rand_quote'{print$0}' quotes |sed 's/\\n/\n'/g| fold -s -w 60
+awk 'NR=='$rand_quote'{print$0}' $quotes_file |sed 's/\\n/\n'/g| fold -s -w 60
 echo
