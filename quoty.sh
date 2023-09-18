d3c5affee6f175093e95c77d08e197e0f13a3a2d
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2023-09-19 02:02:11 +0300

    Update script to use grep instead of wc

--- quoty.sh
+++ quoty.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 quotes_file=~/.config/quoty/quotes
-n_quotes=$(wc -l $quotes_file|cut -d" " -f1)
-rand_quote=$(shuf -i 1-$n_quotes|head -n 1)
+n_quotes=$(grep -e '^"' ~/.config/quoty/quotes -c)
+rand_quote=$(shuf --input-range=1-$n_quotes|head -n 1)
 awk 'NR=='$rand_quote'{print$0}' $quotes_file |sed 's/\\n/\n'/g| fold -s -w 60
 echo
